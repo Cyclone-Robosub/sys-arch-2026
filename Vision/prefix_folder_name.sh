4ed2056d7bb523efa57414a5ac15a36fe7cba067
AlundorZhu <ziqzhu@ucdavis.edu>
2026-01-17 18:28:03 -0800

    rename all sub dir

--- Vision/prefix_folder_name.sh
+++ Vision/prefix_folder_name.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-# Get the name of the current directory
-folder_name=$(basename "$PWD")
+# Loop through all subdirectories in the current directory
+for subdir in */; do
+    # Get the subdirectory name without trailing slash
+    folder_name=$(basename "$subdir")
+    
+    cd "$subdir" || continue
+    # Rename all .txt files in the subdirectory with the folder name as prefix
+    rename "s/^/${folder_name}_/" *.jpg *.jpeg *.png
 
-# Rename all files in the current directory with the folder name as prefix
-rename "s/^/${folder_name}_/" *.txt
+    cd ..
+done
