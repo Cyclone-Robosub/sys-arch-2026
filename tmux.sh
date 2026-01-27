0b233cd20ff4774e8244bc260a0a8c30faeff7f1
Ziqiang (Joe) Zhu <ziqzhu@ucdavis.edu>
2026-01-26 20:12:30 -0800

    starting point

---
+++ tmux.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# This script sets up a tmux session 
+
+SESSION="manny"
+
+# start a new tmux session named "manny" in detached mode
+tmux new-session -d -s $SESSION
+
+# set up the first pane running thrust interface
+tmux split-window -h -t $SESSION # split the window horizontally
+tmux send-keys -t $SESSION:0.0 'source install/setup.sh' C-m
+tmux send-keys -t $SESSION:0.0 'ros2 run thrust_interface thrust_interface' C-m
+
+# set up the second pane running mux
+tmux send-keys -t $SESSION:0.1 'source install/setup.sh' C-m
+tmux send-keys -t $SESSION:0.1 'ros2 run soft_mux soft_mux' C-m
+
+# 
