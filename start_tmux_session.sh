f7c69045f7b7f5dd9f8dd0d6e04b4561cd409128
Ziqiang (Joe) Zhu <ziqzhu@ucdavis.edu>
2026-01-27 19:54:33 -0800

    check and debug

--- start_tmux_session.sh
+++ start_tmux_session.sh
@@ -55,9 +55,10 @@ tmux send-keys -t $SESSION:$WINDOW 'cd ~/mediaMTX && ./mediamtx' C-m
 
 # --- ffmpeg ---
 tmux split-window -h -t $SESSION:$WINDOW
+tmux send-keys -t $SESSION:$WINDOW.1 'echo hi' C-m
 tmux send-keys -t $SESSION:$WINDOW.1 'cd ~/recordings' C-m
-tmux send-keys -t $SESSION:$WINDOW.1 \
-    'ffmpeg -f v4l2 -input_format h264 -video_size 1920x1080 -framerate 30 \
+tmux send-keys -t $SESSION:$WINDOW.1 'ffmpeg -f v4l2 -input_format h264 \
+    -video_size 1920x1080 -framerate 30 \
     -fflags +genpts \
     -i /dev/video2 \
     -c:v copy -f rtsp rtsp://localhost:8554/cam \
