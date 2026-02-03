ff4bc92247bd39804000fd842609c5e86f1ce991
Sherry Lei <slolei@ucdavis.edu>
2026-02-02 18:41:52 -0800

    worked on unit tests, issue with reading from serial

--- onboard/src/dvl/test/test_dvl.cpp
+++ onboard/src/dvl/test/test_dvl.cpp
@@ -6,7 +6,10 @@
 
 static constexpr uint8_t VR_TYPE = 'v';
 static constexpr uint8_t DRR_TYPE = 'd';
-static constexpr uint8_t CONFIG_TYPE = 'r';
+static constexpr uint8_t CONFIG_TYPE = 'c';
+
+static constexpr uint8_t DVL_READ = 'r';
+static constexpr uint8_t DVL_WRITE = 'w';
 
 using namespace dvl;
 
@@ -14,6 +17,11 @@ class TestDVLInterface : public::testing::Test {
 protected:
     std::shared_ptr<DVL> node;
     int pipe_fds[2];  // [0] = read end, [1] = write end
+    rclcpp::Subscription<custom_interfaces::msg::VR>::SharedPtr velocity_report_subscriber;
+    custom_interfaces::msg::VR most_recent_velocity_report;
+    rclcpp::Subscription<custom_interfaces::msg::DRR>::SharedPtr drr_report_subscriber;
+    custom_interfaces::msg::DRR most_recent_drr_report;
+
 
     void SetUp() override {
         if (!rclcpp::ok()) {
@@ -33,10 +41,12 @@ protected:
         close(pipe_fds[1]);
     }
 
-    void create_node(){
+    void create_node(uint8_t dvl_mode){
         // Use pipe_fds[1] (write end) as the file descriptor
         // The node will write to it, and we'll read from pipe_fds[0]
-        std::unique_ptr<FD_Interface> pipe_fd = std::make_unique<Direct_FD>(pipe_fds[1]);
+        std::unique_ptr<FD_Interface> pipe_fd;
+        if(dvl_mode == DVL_READ) pipe_fd = std::make_unique<Direct_FD>(pipe_fds[0]);
+        else pipe_fd = std::make_unique<Direct_FD>(pipe_fds[1]);
 
         node = std::make_shared<DVL>(std::move(pipe_fd));
     }
@@ -80,7 +90,7 @@ protected:
         std::string msg;
         get_serial_message(data_type, msg);
         int length = msg.size();
-        write(pipe_fds[1], msg.c_str(), length);
+        write(pipe_fds[1], msg.c_str(), length + 1);
     }
 
     /**
@@ -97,21 +107,35 @@ protected:
                 ",2.000000,1.000000," //valid, altitude, fom
                 "1.000000,2.000000,3.000000,4.000000,5.000000," //covariance
                 "1.000000,2.000000,3.000000,4.000000," //covariance
-                "1,2,3.000000,0,\n"; //time of validity, time of transmission, time, status
+                "1,2,3.000000,0\r\n\r"; //time of validity, time of transmission, time, status
             break;
         case DRR_TYPE:
             msg =
                 "\t\tp,1716814976.000000,0.110000,0.280000,0.040000," //x, y, z, pos_std
-                "15.400000,-1.100000,-0.300000,3,\n"; //roll, pitch, yaw, status
+                "15.400000,-1.100000,-0.300000,3\r\n\r"; //roll, pitch, yaw, status
             break;
         case CONFIG_TYPE:
             msg =
                 "\t\tc,1475.000000,0.000000,y" //speed of sound, mounting rotation offset, acoustic enabled
-                "n,auto,y,\n"; //dark mode enabled, range mode, periodic cycling enabled
+                "n,auto,y\r\n\r"; //dark mode enabled, range mode, periodic cycling enabled
             break;
         default: 
             msg = "error";
         }
+    }
+    void subscribe_velocity_report() {
+        velocity_report_subscriber = node->create_subscription<custom_interfaces::msg::VR> ("velocity_report", 10, std::bind(&TestDVLInterface::velocity_report_callback, this, std::placeholders::_1));
+    }
+    void velocity_report_callback(custom_interfaces::msg::VR velocity_report) {
+        most_recent_velocity_report = velocity_report;    
+    }
+
+    void subscribe_drr_report() {
+        drr_report_subscriber = node->create_subscription<custom_interfaces::msg::DRR>("dead_reck_report", 10, std::bind(&TestDVLInterface::drr_report_callback, this, std::placeholders::_1));
+    }
+
+    void drr_report_callback(custom_interfaces::msg::DRR drr_report){
+        most_recent_drr_report = drr_report;
     }
 };
 
@@ -120,7 +144,7 @@ protected:
  */
 TEST_F(TestDVLInterface, DVLConstruction) {
     ASSERT_NO_THROW({
-        create_node();
+        create_node(DVL_READ);
     });
 
     ASSERT_NE(node, nullptr);
@@ -131,10 +155,10 @@ TEST_F(TestDVLInterface, DVLConstruction) {
  * @brief Test DVL reading Velocity
  */
  TEST_F(TestDVLInterface, ValidReadVelocityReport){
-    create_node();
+    create_node(DVL_READ);
     velocity_report vr;
 
-    //write valid VR data into  pipe_fds[1] 
+    //write valid VR data into pipe_fds[1] 
     write_serial_message(VR_TYPE);
 
     //check if vr was read correctly
@@ -154,7 +178,7 @@ TEST_F(TestDVLInterface, DVLConstruction) {
  * @brief Test DVL reading DRReport
  */
  TEST_F(TestDVLInterface, ValidReadDRReport){
-    create_node();
+    create_node(DVL_READ);
     dead_reck_report drr;
 
     //write valid DRR data into pipe_fds[1]
@@ -172,7 +196,7 @@ TEST_F(TestDVLInterface, DVLConstruction) {
  * @brief Test DVL reading Config
  */
  TEST_F(TestDVLInterface, ValidReadConfiguration){
-    create_node();
+    create_node(DVL_READ);
     config_report config;
 
     //write valid Config data into pipe_fds[1]
@@ -191,24 +215,75 @@ TEST_F(TestDVLInterface, DVLConstruction) {
  /**
  * @brief Test DVL publishing VR
  */
- TEST_F(TestDVLInterface, DVLPublishVR){
-    create_node();
+ TEST_F(TestDVLInterface, DVLPublishVR) {
+    create_node(DVL_READ);
 
     //write valid VR data into pipe_fds[1] 
     write_serial_message(VR_TYPE);
 
-    //check if publisher has published correct data
-    node->publishVR();
+    //subscribe to VR node
+    subscribe_velocity_report();
 
+    rclcpp::executors::SingleThreadedExecutor exec;
+    exec.add_node(node);
+
+    node->publishVR();
+    exec.spin_some();
+
+    /*
+    "\t\tz,1.000000,2.000000,3.000000," //vx, vy, vz
+        ",2.000000,1.000000," //valid, altitude, fom
+    "1.000000,2.000000,3.000000,4.000000,5.000000," //covariance
+                "1.000000,2.000000,3.000000,4.000000," //covariance
+                "1,2,3.000000,0,\n"; //time of validity, time of transmission, time, status
+    */
+    EXPECT_EQ(most_recent_velocity_report.angle_data.twist.linear.x, 1.000000);
+    EXPECT_EQ(most_recent_velocity_report.angle_data.twist.linear.y, 2.000000);
+    EXPECT_EQ(most_recent_velocity_report.angle_data.twist.linear.z, 3.000000);
+    
+    EXPECT_EQ((most_recent_velocity_report.angle_data.covariance)[0], 1.000000);
+    EXPECT_EQ((most_recent_velocity_report.angle_data.covariance)[1], 2.000000);
+    EXPECT_EQ((most_recent_velocity_report.angle_data.covariance)[2], 3.000000);
+    EXPECT_EQ((most_recent_velocity_report.angle_data.covariance)[3], 4.000000);
+    EXPECT_EQ((most_recent_velocity_report.angle_data.covariance)[4], 5.000000);
+    EXPECT_EQ((most_recent_velocity_report.angle_data.covariance)[5], 1.000000);
+    EXPECT_EQ((most_recent_velocity_report.angle_data.covariance)[6], 2.000000);
+    EXPECT_EQ((most_recent_velocity_report.angle_data.covariance)[7], 3.000000);
+    EXPECT_EQ((most_recent_velocity_report.angle_data.covariance)[8], 4.000000);
+    EXPECT_EQ(most_recent_velocity_report.altitude, 2.000000);
+    EXPECT_EQ(most_recent_velocity_report.fom, 1.000000);
+    EXPECT_EQ(most_recent_velocity_report.time, 1);
+    EXPECT_EQ(most_recent_velocity_report.time_of_validity, 2);
+    EXPECT_EQ(most_recent_velocity_report.time_of_transmission, 3);
+    EXPECT_EQ(most_recent_velocity_report.status, 0);
+    EXPECT_EQ(most_recent_velocity_report.valid, 'n');
  }
 
   /**
  * @brief Test DVL publishing DRR
  */
  TEST_F(TestDVLInterface, DVLPublishDRR){
-    //write valid DRR data into the pipe_fds[0] end
+    create_node(DVL_READ);
 
-    //check if publisher has published correct data
+    //write valid DRR data into pipe_fds[1]
+    write_serial_message(DRR_TYPE);
+
+    //subscribe to DRR node
+    subscribe_drr_report();
+
+    rclcpp::executors::SingleThreadedExecutor exec;
+    exec.add_node(node);
+
+    node->publishVR();
+    exec.spin_some();
+
+    //"\t\tp,1716814976.000000,0.110000,0.280000,0.040000," //x, y, z, pos_std
+    //"15.400000,-1.100000,-0.300000,3,\n"; //roll, pitch, yaw, status
+
+    EXPECT_EQ(most_recent_drr_report.time_stamp, 0);
+    EXPECT_EQ(most_recent_drr_report.pos_data.position.x, 1716814976.000000);
+    EXPECT_EQ(most_recent_drr_report.pos_data.position.y, 0.110000);
+    EXPECT_EQ(most_recent_drr_report.pos_data.position.z, 0.280000);
  }
 
   /**
