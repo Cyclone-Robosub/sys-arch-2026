b27be72be72e1254ff5aa7f9583737893a9164d4
William Barber <wobarber@outlook.com>
2026-03-07 17:26:02 -0800

    Adjust thruster limit to 1300/1700

--- onboard/src/thrust_interface/src/thrust_interface.cpp
+++ onboard/src/thrust_interface/src/thrust_interface.cpp
@@ -149,7 +149,7 @@ int main(int argc, char* argv[]) {
     std::vector<int> thrusters = {8, 9, 6, 7, 13, 11, 12, 10};
     std::unique_ptr<FD_Interface> fd = std::make_unique<Pico_FD>("/dev/serial/by-id/usb-MicroPython_Board_in_FS_mode_7327d9a2ecd31892-if00");
     rclcpp::init(argc, argv);
-    auto thrust_interface = std::make_shared<Thrust_Interface>(thrusters, std::move(fd), 1200, 1800);
+    auto thrust_interface = std::make_shared<Thrust_Interface>(thrusters, std::move(fd), 1300, 1700);
     rclcpp::spin(thrust_interface);
     
     rclcpp::shutdown();
