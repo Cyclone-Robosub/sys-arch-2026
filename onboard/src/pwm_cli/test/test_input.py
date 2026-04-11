5dbe9756fdbd1581c7344d527bbe9d91d5b80895
Declan W <dcwhitlock@ucdavis.edu>
2026-04-11 12:58:54 -0700

    Created Skeletons for all Remaining Tests

--- onboard/src/pwm_cli/test/test_input.py
+++ onboard/src/pwm_cli/test/test_input.py
@@ -9,6 +9,7 @@ Ways to see test results:
 '''
 import builtins
 import pytest
+import time
 
 from pwm_cli.cli_console import main
 from pwm_cli.cli_publisher import *
@@ -37,12 +38,16 @@ def catch_output():
 
 '''
 Outputs the next string in a list each time it is called
+If called with "wait {num}" it will wait for that many seconds
 Used to override input() function for testing the main loop of cli_console
 '''
 def input_iterator(outputs: list[str]):
 	iterator = iter(outputs)
 	def my_input(input = None):
-		return next(iterator)
+		next_input = next(iterator)
+		if "wait " in next_input:
+			time.sleep(float(next_input[5:]))
+		return next_input
 	return my_input
 
 # Used to denote the first important value of catch_output, as the info() command prints 24 times on program start
@@ -96,9 +101,55 @@ def test_invalid_powers(simulate_input, catch_output):
 	for out_num in range(0, 5):
 		assert output_list[INFO_OFFSET + out_num] == "Invalid power inputted\n"
 
+
+		# --- VALID INPUT TESTS ---
+
+# Check that default power correctly updates after 'set power' is processed
+def test_set_power(simulate_input, catch_output):
+	# TODO
+	builtins.input = input_iterator(["", "end session"])
+	main()
+
+
+# DESCRIPTION
+def test_current_command(simulate_input, catch_output):
+	# TODO
+	builtins.input = input_iterator(["", "end session"])
+	main()
+
+
+# DESCRIPTION
+def test_custom_powers(simulate_input, catch_output):
+	# TODO
+	builtins.input = input_iterator(["", "end session"])
+	main()
+
+
+# DESCRIPTION
+def test_custom_times(simulate_input, catch_output):
+	# TODO
+	builtins.input = input_iterator(["", "end session"])
+	main()
+
+
+# DESCRIPTION
+def test_robot_commands(simulate_input, catch_output):
+	# TODO
+	builtins.input = input_iterator(["", "end session"])
+	main()
+
+
 def test_repeated_custom_pwm(simulate_input, catch_output):
-	builtins.input = input_iterator(["custom [1500, 1200, 1500, 1200, 1500, 1200, 1500, 1200]", "yes", "current command", "end session"])
+	builtins.input = input_iterator(["custom [1500, 1200, 1500, 1200, 1500, 1200, 1500, 1200]", 
+								  	 "yes", "current command", "end session"])
 	main()
 	output_list = catch_output
 	pwms = ['1500', '1200', '1500', '1200', '1500', '1200', '1500', '1200']
 	assert output_list[INFO_OFFSET] == f"Current Command: Custom pwm {pwms}\n"
+
+
+# DESCRIPTION
+def test_timed_commands(simulate_input, catch_output):
+	# TODO
+	builtins.input = input_iterator(["wait 1", "end session"])
+	main()
