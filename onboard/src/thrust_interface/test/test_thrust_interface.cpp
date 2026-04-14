61e92af63f4212f1458355df4c07397680373371
William Barber <wobarber@outlook.com>
2026-04-13 21:00:38 -0700

    Test failing unit test
    Add a failing unit test and expect the tests to fail

--- onboard/src/thrust_interface/test/test_thrust_interface.cpp
+++ onboard/src/thrust_interface/test/test_thrust_interface.cpp
@@ -147,6 +147,7 @@ TEST_F(TestThrustInterface, NodeConstruction) {
     
     ASSERT_NE(node, nullptr);
     EXPECT_EQ(node->get_name(), std::string("thrust_interface"));
+    ASSERT_TRUE(false);
 }
 
 /**
