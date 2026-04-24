f5c8ec5483d1af8bf16b70e3517606c2ac9da82c
Ziqiang Zhu <ziqzhu@ucdavis.edu>
2026-04-23 22:58:13 -0700

    ill try run this tomorrow

--- Vision/machine_learning/train_yolo.py
+++ Vision/machine_learning/train_yolo.py
@@ -3,9 +3,10 @@ from ultralytics import YOLO
 model = YOLO("yolo26n-pose")
 
 model.train(
-    data="/workspace/Vision/sim_dataset/data.yaml",
-    epochs=10,
+    data="/workspace/Vision/dataset/data.yaml",
+    epochs=50,
     fliplr=0.0,
     save_period=10,
-    batch=20,
+    batch=32,
+    patience=20
 )
