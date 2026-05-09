c5289ecc5822b444d09ed595d447dadfeb28d603
Devansh Katiyar <devanshkat@gmail.com>
2026-05-08 18:06:20 -0700

    Implement reading voltage from sensor

--- onboard/src/simple_bms/src/simple_bms.cpp
+++ onboard/src/simple_bms/src/simple_bms.cpp
@@ -1,3 +1,8 @@
+#include <iostream>
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include <linux/i2c-dev.h>
+#include <unistd.h>
 #include "simple_bms.hpp"
 
 SimpleBMS::SimpleBMS() {
@@ -11,16 +16,42 @@ SimpleBMS::SimpleBMS() {
 void SimpleBMS::bms_callback() {
     // read from channels
     BatteryStatus status = readBMS()
-    // convert valuesP
-
+    // convert values
+    status.voltage = status.voltage * 0.000125f;
+    std::cout << "voltage: " << status.voltage << std::endl;
     // publish to topic
     publish_bms(status);
 }
 
 BatteryStatus SimpleBMS::readBMS(const std::string& I2C_FD) {
     // get reading from sensor
-    BatteryStatus status = {}; // get values 
-    // return
+    BatteryStatus status = {0,0}; // get values 
+    int file;
+    if ((file = open(I2C_FD, O_RDWR))< 0) {
+        std::cout << "error: device not found" << std::endl;
+    }
+    if (ioctl(file, I2C_SLAVE, BMS_ADDRESS) < 0) {
+        std::cout << "error: Failed to connect" << std::endl;
+    }
+    // config sensor
+    uint8_t config[3];
+    config[0] = 0x01;
+    config[1] = 0xC3;
+    config[2] = 0x83;
+    if (write(file, config, 3) != 3) {
+        std::cout << "error: could not write to config" << std::endl;
+    }
+    // wait for conversion
+    usleep(10000);
+    // read from conversion register
+    uint8_t conv_reg[1] = {0x00};
+    uint8_t output[2];
+    if (read(file, output, 2) != 2) {
+        std::cout << "error: could not read from conversion register" << std::endl;
+    }
+    // reconstruct voltage from output
+    status.voltage = (output[0] << 8) | output[1];
+    // todo get current
     return status;
 }
 
