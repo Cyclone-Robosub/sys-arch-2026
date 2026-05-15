d02caa49606ea00b81bb317d25138ec5d746fbfd
William Barber <wobarber@outlook.com>
2026-05-15 10:16:24 -0700

    Restore Pico ID

--- onboard/src/thrust_interface/src/thrust_interface.cpp
+++ onboard/src/thrust_interface/src/thrust_interface.cpp
@@ -164,7 +164,7 @@ int Pico_FD::open_file() {
 
 int main(int argc, char* argv[]) {
     std::vector<int> thrusters = {8, 9, 6, 7, 13, 11, 12, 10};
-    std::unique_ptr<FD_Interface> fd = std::make_unique<Pico_FD>("/dev/serial/by-id/usb-MicroPython_Board_in_FS_mode_7327d9a2ecd31892");
+    std::unique_ptr<FD_Interface> fd = std::make_unique<Pico_FD>("/dev/serial/by-id/usb-MicroPython_Board_in_FS_mode_7327d9a2ecd31892-if00");
     rclcpp::init(argc, argv);
     auto thrust_interface = std::make_shared<Thrust_Interface>(thrusters, std::move(fd), 1100, 1900);
     rclcpp::spin(thrust_interface);
