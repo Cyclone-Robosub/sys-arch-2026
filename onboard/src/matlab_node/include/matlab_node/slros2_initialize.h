0c90e645e4fbbfd5e2990184d5b9d46df898b22d
abrzamora <alexbrianzamora@gmail.com>
2026-07-07 20:14:01 -0700

    added matlab-node

---
+++ onboard/src/matlab_node/include/matlab_node/slros2_initialize.h
@@ -0,0 +1,56 @@
+// Copyright 2022-2024 The MathWorks, Inc.
+// Generated 07-Jul-2026 19:47:03
+#ifndef _SLROS2_INITIALIZE_H_
+#define _SLROS2_INITIALIZE_H_
+#include "matlab_node_types.h"
+// Generic pub-sub header
+#include "slros2_generic_pubsub.h"
+#ifndef SET_QOS_VALUES
+#define SET_QOS_VALUES(qosStruct, _history, _depth, _durability, _reliability, _deadline \
+, _lifespan, _liveliness, _lease_duration, _avoid_ros_namespace_conventions)             \
+    {                                                                                    \
+        qosStruct.history = _history;                                                    \
+        qosStruct.depth = _depth;                                                        \
+        qosStruct.durability = _durability;                                              \
+        qosStruct.reliability = _reliability;                                            \
+        qosStruct.deadline.sec = _deadline.sec;                                          \
+        qosStruct.deadline.nsec = _deadline.nsec;                                        \
+        qosStruct.lifespan.sec = _lifespan.sec;                                          \
+        qosStruct.lifespan.nsec = _lifespan.nsec;                                        \
+        qosStruct.liveliness = _liveliness;                                              \
+        qosStruct.liveliness_lease_duration.sec = _lease_duration.sec;                   \
+        qosStruct.liveliness_lease_duration.nsec = _lease_duration.nsec;                 \
+        qosStruct.avoid_ros_namespace_conventions = _avoid_ros_namespace_conventions;    \
+    }
+#endif
+inline rclcpp::QoS getQOSSettingsFromRMW(const rmw_qos_profile_t& qosProfile) {
+    rclcpp::QoS qos(rclcpp::QoSInitialization::from_rmw(qosProfile));
+    if (RMW_QOS_POLICY_DURABILITY_TRANSIENT_LOCAL == qosProfile.durability) {
+        qos.transient_local();
+    } else {
+        qos.durability_volatile();
+    }
+    if (RMW_QOS_POLICY_RELIABILITY_RELIABLE == qosProfile.reliability) {
+        qos.reliable();
+    } else {
+        qos.best_effort();
+    }
+    return qos;
+}
+// matlab_node/Subsystem Reference1/Publish
+extern SimulinkPublisher<custom_interfaces::msg::Debug,SL_Bus_custom_interfaces_Debug> Pub_matlab_node_266_3;
+// matlab_node/mission_manager_interface_cgn/Subsystem/Publish
+extern SimulinkPublisher<custom_interfaces::msg::Result,SL_Bus_custom_interfaces_Result> Pub_matlab_node_108_269;
+// matlab_node/pwm_heartbeat_publisher/Publish2
+extern SimulinkPublisher<std_msgs::msg::Empty,SL_Bus_std_msgs_Empty> Pub_matlab_node_37_150;
+// matlab_node/pwm_publisher/Publish
+extern SimulinkPublisher<custom_interfaces::msg::Pwms,SL_Bus_custom_interfaces_Pwms> Pub_matlab_node_38_2;
+// matlab_node/DRR
+extern SimulinkSubscriber<custom_interfaces::msg::DRR,SL_Bus_custom_interfaces_DRR> Sub_matlab_node_3;
+// matlab_node/IMU
+extern SimulinkSubscriber<custom_interfaces::msg::Imu,SL_Bus_custom_interfaces_Imu> Sub_matlab_node_8;
+// matlab_node/VR
+extern SimulinkSubscriber<custom_interfaces::msg::VR,SL_Bus_custom_interfaces_VR> Sub_matlab_node_20;
+// matlab_node/mission_manager_interface_cgn/Subscribe
+extern SimulinkSubscriber<custom_interfaces::msg::Goal,SL_Bus_custom_interfaces_Goal> Sub_matlab_node_108_265;
+#endif
