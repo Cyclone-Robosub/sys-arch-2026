34fcdb69cb34b1357c400c2a7f772540bcc9e0ce
abrzamora <alexbrianzamora@gmail.com>
2026-07-11 19:13:56 -0700

    wip for simple_bms  added test_simple_bms

---
+++ onboard/src/simple_bms/test/test_simple_bms.cpp
@@ -0,0 +1,18 @@
+#include <gtest/gtest.h>
+#include <rclcpp/rclcpp.hpp>
+
+#include "simple_bms.hpp"
+
+#include <chrono>
+
+
+/**
+* @brief Test code for Simple_BMS node
+* This code will create a mock serial device to test the
+* simple bms without requiring actual hardware.
+*/
+
+class TestSimpleBMS : public ::testing::Test {
+protected:
+    std::shared_ptr<simple_bms> node;
+}
