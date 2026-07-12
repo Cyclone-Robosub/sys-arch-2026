0b1bbcbd5c3d27275332965e8fa77413d718337f
frank-ucd <fofaulkner@ucdavis.edu>
2026-07-12 01:08:56 -0700

    Hopefully added current detection

--- onboard/src/simple_bms/include/simple_bms.hpp
+++ onboard/src/simple_bms/include/simple_bms.hpp
@@ -24,16 +24,50 @@ class SimpleBMS : public rclcpp::Node {
         ~SimpleBMS();
 
     private:
+        struct ConfigValues {
+            uint8_t conf_reg; // Config register
+            uint8_t lsb; // See pg 25-26 of the ads1115.pdf (or below) for what the individual bits mean
+            uint8_t msb;
+            uint8_t conv_reg_val;
+        };
+        
         const char* I2C_FD = "/dev/i2c-1";
         const int BMS_ADDRESS = 0x48;
+        const ConfigValues VOLTAGE_CONFIG = { // FIXME confirm that the multiplexor configurations should be 000 and 010 and not 100 and 101
+            // FIXME the below line seems to imply that I don't need to do all the config changing stuff
+            // AIN_P and AIN_N denote the selected positive and negative inputs.
+            /* Value of 8342 -- 1000001101000010
+            1   - Start a single conversion
+            000 - Input multiplexer configuration - AIN_P = AIN0 and AIN_N = AIN1
+            001 - FSR=+- 4.096V
+            1   - Operate in single-shot mode
+            010 - Data rate of 32SPS
+            0   - Comparator operating mode: Traditional
+            0   - Alert/ready pin polarity: Active low
+            0   - Assert pin does not latch
+            10  - Assert the alert/ready pin after four consecutive conversions which exceed the limit
+            */
+            0x01,
+            0x42,
+            0x83,
+            0x00
+        };
+        const ConfigValues CURRENT_CONFIG = {
+            // Only change from the values in the voltage config is bit 13 such that the multiplexer config is 010, meaning AIN_P = AIN1 and AIN_N = AIN3
+            0x01,
+            0x42,
+            0xA3,
+            0x00
+        };
         int i2c_fd = -1;
         
         rclcpp::TimerBase::SharedPtr battery_timer;
         rclcpp::Publisher<custom_interfaces::msg::Battery>::SharedPtr bms_publisher;
 
         bool init_i2c();
+        bool config_i2c(SimpleBMS::ConfigValues conf);
         void bms_callback();
-        BatteryStatus readBMS();
+        float readBMS(bool isVoltage);
         void publish_bms(BatteryStatus status);
 };
 
--- onboard/src/simple_bms/src/simple_bms.cpp
+++ onboard/src/simple_bms/src/simple_bms.cpp
@@ -5,7 +5,8 @@ using namespace std::chrono_literals;
 SimpleBMS::SimpleBMS() : Node("simple_bms") {
     if (init_i2c()){
         // Timer to check voltage & current values at a frequency of 100 hz
-        battery_timer = this->create_wall_timer(10ms, std::bind(&SimpleBMS::bms_callback, this));
+        // FIXME set this to 60ms temporarily to make sure I am not running into issues, this should be changed down once I understand what is happening better
+        battery_timer = this->create_wall_timer(60ms, std::bind(&SimpleBMS::bms_callback, this)); // This has been changed to 20ms from 10ms since it needs to wait 10ms in between reading the voltage and the current
     }
     // Publisher to Battery topic
     bms_publisher = this->create_publisher<custom_interfaces::msg::Battery>("bms", 10);
@@ -18,6 +19,7 @@ SimpleBMS::~SimpleBMS() {
 }
 
 bool SimpleBMS::init_i2c() {
+    // Open and connect to the ADC
     if ((i2c_fd = open(I2C_FD, O_RDWR))< 0) {
         std::cout << "error: device not found" << std::endl;
         return false;
@@ -26,19 +28,27 @@ bool SimpleBMS::init_i2c() {
         std::cout << "error: Failed to connect" << std::endl;
         return false;
     }
-    // config sensor
+    
+    return config_i2c(VOLTAGE_CONFIG);
+    
+}
+
+bool SimpleBMS::config_i2c(SimpleBMS::ConfigValues conf){
     uint8_t config[3];
-    config[0] = 0x01;
-    config[1] = 0x42;
-    config[2] = 0x83;
+    config[0] = conf.conf_reg;
+    config[1] = conf.lsb;
+    config[2] = conf.msb;
+
     if (write(i2c_fd, config, 3) != 3) {
         std::cout << "error: could not write to config" << std::endl;
         return false;
     }
+
     // wait for conversion
-    usleep(10000);
+    usleep(10000); //10ms in microseconds
+
     // read from conversion register
-    uint8_t conv_reg[1] = {0x00};
+    uint8_t conv_reg[1] = {conf.conv_reg_val};
     if (write(i2c_fd, conv_reg, 1) != 1) {
         std::cerr << "Failed to set pointer to conversion register." << std::endl;
         return false;
@@ -48,25 +58,35 @@ bool SimpleBMS::init_i2c() {
 
 // contains all function calls that should be performed 
 void SimpleBMS::bms_callback() {
-    // read from channels
-    SimpleBMS::BatteryStatus status = readBMS();
-    // publish to topic
+    // FIXME add a variable here for safety to make sure it is on the correct i2c configuration
+
+    SimpleBMS::BatteryStatus status = {0.0f,0.0f};
+
+    // read voltage. i2c is configured to read voltage upon init and at the end of the bms_callback here 
+    status.voltage = readBMS(true);
+
+    // configure and read current
+    if (!config_i2c(CURRENT_CONFIG)) return; // No need for an error message here; that is handled in config_current
+    status.current = readBMS(false);
+
+    // publish to topic before resetting back to the configuration for voltage, just in case the configuration fails
     publish_bms(status);
+
+    // Configure for reading voltage on the next callback
+    if (!config_i2c(VOLTAGE_CONFIG)) return;
 }
 
-SimpleBMS::BatteryStatus SimpleBMS::readBMS() {
-    // get reading from sensor
-    SimpleBMS::BatteryStatus status = {0.0f,0.0f};
+float SimpleBMS::readBMS(bool isVoltage) {
+    // get reading from sensor. This will either be Voltage on pin A0 or current on pin A1, depending on how it was configured before this function was called
+
     uint8_t output[2];
     if (read(i2c_fd, output, 2) != 2) {
         std::cout << "error: could not read from conversion register" << std::endl;
-        return status;
+        return 0.0f;
     }
-    // reconstruct voltage from output
-    int16_t voltage = (output[0] << 8) | output[1];
-    status.voltage = voltage * 0.000125f;
-    // TODO get current
-    return status;
+    // reconstruct value from output, using the relevant scalar depending on whether it is reading current or voltage
+    int16_t combined_out = (output[0] << 8) | output[1];
+    return combined_out * ((isVoltage) ? 0.000125f : 1.0f);
 }
 
 void SimpleBMS::publish_bms(SimpleBMS::BatteryStatus status) {
